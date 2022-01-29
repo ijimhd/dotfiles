df914a8169865fc3430bc933cf47b88a32365b46
ijimhd <75863574+ijimhd@users.noreply.github.com>
2022-01-29 12:25:57 +0200

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,5 +3,5 @@ pidgin &
 nitrogen --restore &
 setxkbmap -layout us,gr &
 setxkbmap -option 'grp:alt_shift_toggle' &
-xinput set-prop 13 340 1 &
-xinput set-prop 13 320 1 &
+xinput set-prop 14 342 1 &
+xinput set-prop 14 320 1 &
